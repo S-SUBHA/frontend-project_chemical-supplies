395869893d70e23e59540582fb5019f6481b69af
S-SUBHA <cesubhadip@gmail.com>
2024-10-02 03:36:41 +0530

    Bug fix: addRow.feature.js

--- src/features/addRow.feature.js
+++ src/features/addRow.feature.js
@@ -1,6 +1,6 @@
 export function addRow(data) {
   const row = {
-    id: data.slice(-1)[0].id + 1,
+    id: data.length ? data.slice(-1)[0].id + 1 : 1,
     "Chemical name": "",
     Vendor: "",
     Density: "",
