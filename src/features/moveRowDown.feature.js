6d086f1656cbe69844c03b5151a832aa909d48b5
S-SUBHA <cesubhadip@gmail.com>
2024-10-02 03:29:07 +0530

    Add styling

--- src/features/moveRowDown.feature.js
+++ src/features/moveRowDown.feature.js
@@ -3,7 +3,7 @@ import { getCheckedRows } from "../utils/index.js";
 export function moveRowDown(element, data){
   const checkedRows = getCheckedRows(element);
   const checkedRowsIndices = checkedRows.map((row) => Number(row.id));
-  console.log(checkedRowsIndices);
+  // console.log(checkedRowsIndices);
 
   for (let i = data.length - 2; i >= 0; i--) {
     if (!checkedRowsIndices.includes(i)) continue;
--- src/features/moveRowUp.feature.js
+++ src/features/moveRowUp.feature.js
@@ -3,7 +3,7 @@ import { getCheckedRows } from "../utils/index.js";
 export function moveRowUp(element, data){
   const checkedRows = getCheckedRows(element);
   const checkedRowsIndices = checkedRows.map((row) => Number(row.id));
-  console.log(checkedRowsIndices);
+  // console.log(checkedRowsIndices);
 
   for (let i = 1; i < data.length; i++) {
     if (!checkedRowsIndices.includes(i)) continue;
