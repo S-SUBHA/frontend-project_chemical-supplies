a40b73460dbf88cde430e0c019f530a0789324e5
S-SUBHA <cesubhadip@gmail.com>
2024-10-03 22:57:21 +0530

    Rename 'styles' folder to 'css' and bug-fix: getData.util.js

--- src/utils/getData.util.js
+++ src/utils/getData.util.js
@@ -3,9 +3,7 @@ import { getFromLocalStorage } from "./index.js";
 export async function getData() {
   const data =
     getFromLocalStorage("chemicalSupplies") ??
-    (await fetch(
-      "https://s-subha.github.io/frontend-project_chemical-supplies/data/data.json"
-    )
+    (await fetch("data/data.json")
       .then((res) => res.json())
       .catch((error) => console.log(`Error fetching Data: ${error}`)));
 
